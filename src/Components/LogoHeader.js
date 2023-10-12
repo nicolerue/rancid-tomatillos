97a3e5eacda4b69a6198c3266c00907778d6852b
Nicole Rue <nicolerue92@gmail.com>
2023-10-11 18:45:45 -0700

    Initial Commit

---
+++ src/Components/LogoHeader.js
@@ -0,0 +1,9 @@
+function LogoHeader() {
+  return (
+    <div>
+      <h1>Logo and Header</h1>
+    </div>
+  );
+}
+
+export default LogoHeader;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import "./NavBar.scss";
+import LogoHeader from "./LogoHeader";
+import SearchBar from "./SearchBar";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <LogoHeader />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default NavBar;
