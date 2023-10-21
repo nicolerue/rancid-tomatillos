2550bdc088b4ae471801babac3e9b550d8736caf
Nicole Rue <nicolerue92@gmail.com>
2023-10-20 19:01:46 -0700

    edit api links

--- src/Components/apiCalls.js
+++ src/Components/apiCalls.js
@@ -1,23 +1,23 @@
 export function getMoviesFromApiDisplay() {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(
-    res => {
+  return fetch("http://rancid-tomatillos.herokuapp.com/api/v2/movies").then(
+    (res) => {
       if (!res.ok) {
         throw new Error(
-          'Oops! Something went wrong on the server. Please try again later.',
+          "Oops! Something went wrong on the server. Please try again later."
         );
       } else {
         return res.json();
       }
-    },
+    }
   );
 }
 export function getSingleMovieApi(id) {
   return fetch(
-    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`,
-  ).then(res => {
+    `http://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+  ).then((res) => {
     if (!res.ok) {
       throw new Error(
-        'Oops! Something went wrong on the server. Please try again later.',
+        "Oops! Something went wrong on the server. Please try again later."
       );
     } else {
       return res.json();
@@ -27,11 +27,11 @@ export function getSingleMovieApi(id) {
 
 export function getSingleMovieVideoApi(id) {
   return fetch(
-    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`,
-  ).then(res => {
+    `http://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`
+  ).then((res) => {
     if (!res.ok) {
       throw new Error(
-        'Oops! Something went wrong on the server. Please try again later',
+        "Oops! Something went wrong on the server. Please try again later"
       );
     } else {
       return res.json();
