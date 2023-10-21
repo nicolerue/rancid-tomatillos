e2b81804287966a4f4e13e057c37901e55cd64f4
Nicole Rue <nicolerue92@gmail.com>
2023-10-20 18:24:44 -0700

    test

--- src/Components/LogoHeader.js
+++ src/Components/LogoHeader.js
@@ -1,11 +1,11 @@
-import tomatoImage from '../tomato2.avif';
-import './LogoHeader.scss';
+import tomatoImage from "../tomato2.avif";
+import "./LogoHeader.scss";
 
 function LogoHeader() {
   return (
     <div className="header">
       <img className="tomato-logo" src={tomatoImage} alt="tomato-logo" />
-      <h1>Rancid Tomatillos</h1>
+      <h1>Jen and Nicole</h1>
     </div>
   );
 }
