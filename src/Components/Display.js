0182756efe2b4156b1b7ae80976ab7e39b153560
Nicole Rue <nicolerue92@gmail.com>
2023-10-21 13:10:23 -0700

    fetch2

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Movie from './Movie';
-import { getMoviesFromApiDisplay } from './apiCalls';
-import './Display.scss';
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import Movie from "./Movie";
+import { getMoviesFromApiDisplay } from "./apiCalls";
+import "./Display.scss";
 
 function Display({
   setModalIsOpen,
@@ -13,21 +13,21 @@ function Display({
   setError,
   setApiMovieData,
 }) {
-  useEffect(() => {
-    getMoviesFromApiDisplay()
-      .then(data => {
-        setApiMovieData(data.movies);
-      })
-      .catch(error => {
-        setError(error.message || 'An unknown error occurred.');
-      });
-    setApiMovieData(apiMovieData);
-    setModalIsOpen(false);
-  }, []);
+  // useEffect(() => {
+  //   getMoviesFromApiDisplay()
+  //     .then(data => {
+  //       setApiMovieData(data.movies);
+  //     })
+  //     .catch(error => {
+  //       setError(error.message || 'An unknown error occurred.');
+  //     });
+  //   setApiMovieData(apiMovieData);
+  //   setModalIsOpen(false);
+  // }, []);
 
   return (
     <div className="movie-display">
-      {apiMovieData.map(movie => {
+      {apiMovieData.map((movie) => {
         return (
           <Movie
             key={movie.id}
