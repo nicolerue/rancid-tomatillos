6d4a3bd4b98599364b9e9ee406a42ab112d5393c
Nicole Rue <nicolerue92@gmail.com>
2023-10-20 18:18:56 -0700

    not found page

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
             />
           }
         />
-        <Route path="*" element={<PageNotFound />} />
+        {/* <Route path="*" element={<PageNotFound />} /> */}
       </Routes>
     </main>
   );
